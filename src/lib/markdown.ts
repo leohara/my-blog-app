582efe4f311478083fc63b8b23b1348f81b57d2c
leohara <engbeatleos@gmail.com>
2025-07-13 23:56:48 +0900

    feat: syntax-highlight

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,6 +1,8 @@
 import { remark } from "remark";
 import remarkGfm from "remark-gfm";
-import remarkHtml from "remark-html";
+import remarkRehype from "remark-rehype";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeStringify from "rehype-stringify";
 import { remarkLinkCard } from "./remark-link-card";
 
 /**
@@ -12,7 +14,16 @@ export async function markdownToHtml(markdown: string): Promise<string> {
   const result = await remark()
     .use(remarkGfm) // GitHub Flavored Markdownのサポート
     .use(remarkLinkCard) // リンクカードプラグインを追加
-    .use(remarkHtml) // HTMLへの変換
+    .use(remarkRehype) // RemarkからRehypeへ変換
+    .use(rehypePrettyCode, {
+      // テーマ設定
+      theme: "one-dark-pro",
+      // 言語がない場合のデフォルト
+      defaultLang: "plaintext",
+      // コードブロックに data-language 属性を追加
+      keepBackground: true,
+    }) // シンタックスハイライト
+    .use(rehypeStringify) // HTMLへの変換
     .process(markdown);
 
   // マーカーをHTMLに変換
